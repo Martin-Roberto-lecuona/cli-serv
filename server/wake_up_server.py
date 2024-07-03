c08513f7761381cf8e93ee0478bd198cb2c51f6a
Martin-Roberto-lecuona <robertlecuona@gmail.com>
2024-07-03 11:17:47 -0300

    Update wake_up_server.py

--- server/wake_up_server.py
+++ server/wake_up_server.py
@@ -2,9 +2,15 @@ import os
 import platform
 import threading
 
+
+command_server = "python3 server_api.py"
+command_ngrok = "ngrok http http://localhost:8000"
+
 def manageCommand(command): 
+    global command_server
     current_os = platform.system()
     if current_os == "Windows":
+        command_server = "python server_api.py"
         os.system(f'start cmd /k "{command}"')
     elif current_os == "Linux":
         os.system(f'gnome-terminal -- bash -c "{command}; exec bash"')
@@ -14,8 +20,6 @@ def manageCommand(command):
     #print(f"The command '{command}' exited with code: {exit_code}")
 
 
-command_server = "python3 server_api.py"
-command_ngrok = "ngrok http http://localhost:8000"
 
 
 hilo_serv = threading.Thread(target=manageCommand, args=(command_server,))
